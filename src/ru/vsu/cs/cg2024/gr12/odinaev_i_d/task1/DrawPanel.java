5a1176ceee088d6e0b9b259eb18db7acd2c0c525
Ilya <odinaev.id2005@gmail.com>
2024-09-19 21:50:32 +0300

    Прорисовка горы. Узор - нарисован, но - доделать!

--- src/ru/vsu/cs/cg2024/gr12/odinaev_i_d/task1/DrawPanel.java
+++ src/ru/vsu/cs/cg2024/gr12/odinaev_i_d/task1/DrawPanel.java
@@ -1,5 +1,6 @@
 package ru.vsu.cs.cg2024.gr12.odinaev_i_d.task1;
 
+import ru.vsu.cs.cg2024.gr12.odinaev_i_d.task1.elements.Montain;
 import ru.vsu.cs.cg2024.gr12.odinaev_i_d.task1.elements.Sky;
 
 import javax.swing.*;
@@ -11,18 +12,23 @@ public class DrawPanel extends JPanel implements ActionListener {
     Timer timer;
 
     private final Sky sky;
+    private final Montain montain;
     //Поля
 
     public DrawPanel(int timerDelay) {
         timer = new Timer(timerDelay, this);
         timer.start();
-        sky = new Sky(0,0,600,800,5, Color.BLUE);
+        sky = new Sky(0,0,600,300,5, Color.WHITE);
+        montain = new Montain(0, 800, 750, 550);
     }
     @Override
     public void paint(Graphics g) {
+        setBackground(Color.decode("#E38756"));
         Graphics2D g2d = (Graphics2D) g;
         super.paint(g2d);
+        sky.setWidth(getWidth());
         sky.draw(g2d);
+        montain.draw(g2d);
     }
     public void actionPerformed(final ActionEvent e) {
         if (e.getSource() == timer) {
